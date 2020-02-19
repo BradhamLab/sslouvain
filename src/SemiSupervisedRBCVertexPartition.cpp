c0aa2cf95e811e9f71958d4992e3e9bb59ea6eaa
dakota-hawkins <dakotahawkins0110@gmail.com>
2020-02-18 21:12:02 -0500

    properly sizing mutable vector.

--- src/SemiSupervisedRBCVertexPartition.cpp
+++ src/SemiSupervisedRBCVertexPartition.cpp
@@ -23,6 +23,10 @@ SemiSupervisedRBCVertexPartition::SemiSupervisedRBCVertexPartition(Graph* graph,
                                                                    double resolution_parameter,
                                                                    vector<bool> const& mutables) :
   RBConfigurationVertexPartition(graph, membership, resolution_parameter) {
+    this -> _mutables = vector(graph -> vcount(), true);
+    if (mutables.size() != graph -> vcount()) {
+      throw Exception("Mutable vector has incorrect size.");
+    }
     this -> set_mutable(mutables);
   }
 
@@ -30,23 +34,26 @@ SemiSupervisedRBCVertexPartition::SemiSupervisedRBCVertexPartition(Graph* graph,
                                                                    vector<size_t> const& membership,
                                                                    vector<bool> const& mutables) :
   RBConfigurationVertexPartition(graph, membership) {
+    this -> _mutables = vector(graph -> vcount(), true);
     this -> set_mutable(mutables);
 }
 
 SemiSupervisedRBCVertexPartition::SemiSupervisedRBCVertexPartition(Graph* graph,
                                                                    vector<size_t> const& membership) :
   RBConfigurationVertexPartition(graph, membership) {
-    this -> set_mutable();
+    this -> _mutables = vector(graph -> vcount(), true);
   }
 
 SemiSupervisedRBCVertexPartition::SemiSupervisedRBCVertexPartition(Graph* graph,
                                                                    double resolution_parameter) :
   RBConfigurationVertexPartition(graph, resolution_parameter) {
+    this -> _mutables = vector(graph -> vcount(), true); 
     this -> set_mutable();
   }
 
 SemiSupervisedRBCVertexPartition::SemiSupervisedRBCVertexPartition(Graph* graph) : 
   RBConfigurationVertexPartition(graph) {
+    this -> _mutables = vector(graph -> vcount(), true);
     this -> set_mutable();
   }
 
@@ -112,71 +119,4 @@ vector<bool> SemiSupervisedRBCVertexPartition::collapse_mutables() {
     }
   }
   return(collapsed);
-}
-
-void SemiSupervisedRBCVertexPartition::renumber_communities()
-{
-  vector<SemiSupervisedRBCVertexPartition*> partitions(1);
-  partitions[0] = this;
-  this->renumber_communities(SemiSupervisedRBCVertexPartition::renumber_communities(partitions));
-}
-
-void SemiSupervisedRBCVertexPartition::renumber_communities(vector<size_t> const& membership)
-{
-  this->set_membership(membership);
-}
-
-
-vector<size_t> SemiSupervisedRBCVertexPartition::renumber_communities(vector<SemiSupervisedRBCVertexPartition*> partitions)
-{
-  size_t nb_layers = partitions.size();
-  size_t nb_comms = partitions[0]->nb_communities();
-  size_t n = partitions[0]->graph->vcount();
-
-  #ifdef DEBUG
-    for (size_t layer; layer < nb_layers; layer++)
-    {
-      for (size_t v = 0; v < n; v++)
-      {
-        if (partitions[0]->membership(v) != partitions[layer]->membership(v))
-          cerr << "Membership of all partitions are not equal";
-      }
-    }
-  #endif
-  // First sort the communities by size
-  // Csizes
-  // first - community
-  // second - csize
-  // third - number of nodes (may be aggregate nodes), to account for communities with zero weight.
-  vector<size_t*> csizes;
-  for (size_t i = 0; i < nb_comms; i++)
-  {
-      size_t csize = 0;
-      for (size_t layer = 0; layer < nb_layers; layer++)
-        csize += partitions[layer]->csize(i);
-
-      size_t* row = new size_t[3];
-      row[0] = i;
-      row[1] = csize;
-      row[2] = partitions[0]->community[i]->size();
-      csizes.push_back(row);
-  }
-  sort(csizes.begin(), csizes.end(), orderCSize);
-
-
-  // Then use the sort order to assign new communities,
-  // such that the largest community gets the lowest index.
-  vector<size_t> new_comm_id(nb_comms, 0);
-  for (size_t i = 0; i < nb_comms; i++)
-  {
-    size_t comm = csizes[i][0];
-    new_comm_id[comm] = i;
-    delete[] csizes[i];
-  }
-
-  vector<size_t> membership(n, 0);
-  for (size_t i = 0; i < n; i++)
-    membership[i] = new_comm_id[partitions[0]->_membership[i]];
-
-  return membership;
 }
